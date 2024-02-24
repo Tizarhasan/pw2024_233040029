e95f33131b9c15f9c9b55da91f08e76cfc74c963
tizarhasan <tizarhasan@Zar-MacBook-Pro.local>
2024-02-24 07:05:07 +0700

    Mengumpulkan tugas

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,25 @@
+<?php
+$first_name = "Tyezar";
+$middle_name = "Hasan";
+$last_name = "Khusaeni";
+echo "Hello My Name Is, $first_name $middle_name $last_name<br>";
+
+$angka = 10;
+$result = $angka * 2;
+echo "I'm $result Years Old<br>";
+
+$hobby = "Videography";
+echo "My Hobby Is, $hobby<br>";
+
+$student = "Universitas Pasundan";
+echo "Currently I,m Studying At $student<br><br><br><br><br><br>";
+
+$latihan = "Latihan 2";
+$nama = "Tyezar";
+$umur = "20";
+$kampus = "Universitas Pasundan";
+echo "$latihan<br>";
+echo "Nama Saya $nama. Umur saya $umur, saya kuliah di $kampus";
+
+
+?>
